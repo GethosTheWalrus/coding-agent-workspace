c83093054f4570b2cfb5d8d0e3b0cfc9e4e5dc60
Michael Toscano <7661715+GethosTheWalrus@users.noreply.github.com>
2026-02-23 19:17:07 -0500

    Fix imports in Todo router

--- app/api/todo.py
+++ app/api/todo.py
@@ -11,42 +11,47 @@ Endpoints:
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from app import crud, schemas, models
+from app.crud import todo as crud_todo
+from app.schemas.todo import (
+    TodoResponse,
+    TodoCreate,
+    TodoUpdate,
+)
 from app.db.base import get_db
 
 router = APIRouter()
 
 # List todos
-@router.get("/", response_model=list[schemas.todo.TodoResponse])
+@router.get("/", response_model=list[TodoResponse])
 async def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    todos = crud.todo.get_todos(db, skip=skip, limit=limit)
+    todos = crud_todo.get_todos(db, skip=skip, limit=limit)
     return todos
 
 # Get a single todo
-@router.get("/{todo_id}", response_model=schemas.todo.TodoResponse)
+@router.get("/{todo_id}", response_model=TodoResponse)
 async def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = crud.todo.get_todo(db, todo_id)
+    todo = crud_todo.get_todo(db, todo_id)
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
 # Create a new todo
-@router.post("/", response_model=schemas.todo.TodoResponse, status_code=status.HTTP_201_CREATED)
-async def create_todo(todo_in: schemas.todo.TodoCreate, db: Session = Depends(get_db)):
-    return crud.todo.create_todo(db, todo_in)
+@router.post("/", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_in: TodoCreate, db: Session = Depends(get_db)):
+    return crud_todo.create_todo(db, todo_in)
 
 # Update an existing todo
-@router.put("/{todo_id}", response_model=schemas.todo.TodoResponse)
-async def update_todo(todo_id: int, todo_in: schemas.todo.TodoUpdate, db: Session = Depends(get_db)):
-    db_todo = crud.todo.get_todo(db, todo_id)
+@router.put("/{todo_id}", response_model=TodoResponse)
+async def update_todo(todo_id: int, todo_in: TodoUpdate, db: Session = Depends(get_db)):
+    db_todo = crud_todo.get_todo(db, todo_id)
     if not db_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
-    return crud.todo.update_todo(db, db_todo, todo_in)
+    return crud_todo.update_todo(db, db_todo, todo_in)
 
 # Delete a todo
-@router.delete("/{todo_id}", response_model=schemas.todo.TodoResponse)
+@router.delete("/{todo_id}", response_model=TodoResponse)
 async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    db_todo = crud.todo.get_todo(db, todo_id)
+    db_todo = crud_todo.get_todo(db, todo_id)
     if not db_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
-    return crud.todo.delete_todo(db, db_todo)
+    return crud_todo.delete_todo(db, db_todo)
