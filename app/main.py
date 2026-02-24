8b20eb27441d542b3fe86ff40838054a8749eb68
Michael Toscano <7661715+GethosTheWalrus@users.noreply.github.com>
2026-02-23 22:39:43 -0500

    Add startup event to create tables and import Base, engine

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from app.routers import todo
+from app.db import Base, engine
 
 
 def get_application() -> FastAPI:
     app = FastAPI(title="Todo API")
     app.include_router(todo.router)
+
+    @app.on_event("startup")
+    def on_startup():
+        # Create tables if they don't exist
+        Base.metadata.create_all(bind=engine)
+
     return app
 
 
