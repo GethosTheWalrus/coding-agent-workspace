9d85653a29f9b2d5245ce6ca421bc5a658b929ea
Michael Toscano <7661715+GethosTheWalrus@users.noreply.github.com>
2026-02-24 11:51:04 -0500

    Add app/main.py placeholder

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from .routers import todo
+
+
+def get_application() -> FastAPI:
+    app = FastAPI(title="Todo API")
+    app.include_router(todo.router)
+    return app
+
+
+app = get_application()
