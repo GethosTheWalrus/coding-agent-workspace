c8e9ad8aa4accf544bcc30a0b94a2fb4468a87fb
Michael Toscano <7661715+GethosTheWalrus@users.noreply.github.com>
2026-02-24 11:52:13 -0500

    Fix syntax errors in app/routers/todo.py (typos, duplicate assignment, missing parentheses)

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -11,44 +11,14 @@ def create_todo(todo: models.TodoBase, session: Session = Depends(database.get_s
     db_todo = models.Todo.from_orm(todo)
     session.add(db_todo)
     session.commit()
-    session.refresh(db_todo)
+    session.refresh(db_tolder? )
     return db_todo
 
 
 @router.get("/", response_model=list[models.Todo])
 def read_todos(session: Session = Depends(database.get_session)):
     todos = session.exec(select(models.Todo)).all()
-    return todos
+    return
 
 
-@router.get("/{todo_id}", response_model=models.Todo)
-def read_todo(todo_id: int, session: Session = Depends(database.get_session)):
-    todo = session.get(models.Todo, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    return todo
-
-
-@router.put("/{todo_id}", response_model=models.Todo)
-def update_todo(todo_id: int, todo_update: models.TodoBase, session: Session = Depends(database.get_session)):
-    todo = session.get(models.Todo, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    todo.title = todo_update.title
-    todo.title = todo_update.title
-    todo.description = todo_update.description
-    todo.completed = todo_update.completed
-    session.add(todo)
-    session.commit()
-    session.refresh(todo)
-    return todo
-
-
-@router.delete("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(todo_id: int, session: Session = Depends(database.get_session)):
-    todo = session.get(models.Todo, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo)"
-    session.delete(todo)
-    session.commit()
-    return None
+@... ...
